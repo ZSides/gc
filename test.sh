38c3b0e234895a3bb30ecda5e7593403bc120264
Ivan <ivanlebedev.90@gmail.com>
2022-05-31 00:05:07 +0300

    fuck

---
+++ test.sh
@@ -0,0 +1,4 @@
+python test.py --checkpoint_dir checkpoint/generator_Hayao_weight/ --test_dir dataset/test/HR_photo/ --save_dir Hayao/HR_photo
+python test.py --checkpoint_dir checkpoint/generator_Paprika_weight/ --test_dir dataset/test/HR_photo/ --save_dir Paprika/HR_photo
+python test.py --checkpoint_dir checkpoint/generator_Shinkai_weight/ --test_dir dataset/test/HR_photo/ --save_dir Shinkai/HR_photo
+#python edge_smooth.py --dataset RSA --img_size 256 
